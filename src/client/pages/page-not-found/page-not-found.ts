d4eb45a36b7f625b8104e0d13340df0b722d1019
Ivan Chernenko <chernenko.ivan17@gmail.com>
2021-02-20 19:34:37 +0300

    add return to profile link on 404 page

--- src/client/pages/page-not-found/page-not-found.ts
+++ src/client/pages/page-not-found/page-not-found.ts
@@ -1,9 +1,15 @@
 import {template} from './template';
 import {Page} from '../../lib/page/index';
+import {Link} from "../../components/link";
 
 export class PageNotFound extends Page<{}> {
     constructor() {
-        super('not-found', {});
+        super('not-found', {}, [
+            new Link({
+                root: 'not-found-return-link',
+                href: '/profile'
+            })
+        ]);
     }
 
     render(): string {
--- src/client/pages/page-not-found/template.ts
+++ src/client/pages/page-not-found/template.ts
@@ -1,6 +1,9 @@
+import {placeholder} from "../../lib/placeholder/index";
+
 export const template = `
 <main class='content content_centered content_column not-found'>
     <h1>404</h1>
     <h2>Страница не найдена :(</h2>
+    ${placeholder('not-found-return-link')}
 </main>
 `;
